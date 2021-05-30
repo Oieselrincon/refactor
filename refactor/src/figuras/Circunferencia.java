392d24d82e46fb168a97b9f06364de84b0757aca
oscar <oscarluisdominguezrivero@alumno.ieselrincon.es>
2021-05-30 10:58:00 +0100

    Documentación del código

--- refactor/src/figuras/Circunferencia.java
+++ refactor/src/figuras/Circunferencia.java
@@ -1,13 +1,29 @@
 package figuras;
 
+/**
+ * Esta clase deonominada Circunferencia, define una Circunferencia, dispone de los atributos radio (double)
+ * y color (String)
+ * 
+ * @author Oscar Domínguez, <a href=
+ *         "mailto:oscarluisdominguezrivero@alumno.ieselrincon.es">enviar correo
+ *         electrónico</a>
+ */
 public class Circunferencia {
 	private double radio;
 	private String color;
 
+	/**
+	 * Métododo de tipo set
+	 * 
+	 * @param radio establece el radio de la circunferencia
+	 */
 	public Circunferencia(double radio) {
 		this.radio = radio;
 	}
-
+	
+	/**
+	 * Método que imprime por pantalla el diámetro, el color y el área de la circunferencia
+	 */
 	public void imprimir() {
 		color = "rojo";
 		System.out.println("Diámetro: " + (2 * radio));
@@ -16,7 +32,14 @@ public class Circunferencia {
 		double area = area1;
 		System.out.println(area);
 	}
-
+	
+	/**
+	 * Métdodo que devuelve un booleano cuando los radios de 2 circunferencias son iguales o 
+	 * la diferencia absoluta es menor a 1, dependiendo de la variable considerarDecimales 
+	 * @param considerarDecimales es una varaible booleana que nos indica si consideramos decimales o no
+	 * @param otro corresponde a un objeto circunferencia con el que vamos a comparar el radio del objeto actual
+	 * @return devuelve verdadero si son iguales o tienen una diferencia menor a 1, según el caso, y falso en otro caso
+	 */
 	public boolean esIgual(boolean considerarDecimales, Circunferencia otro) {
 		double radio1 = this.radio;
 		double radio2 = otro.getRadio();
@@ -34,17 +57,19 @@ public class Circunferencia {
 	}
 
 	/**
-	 * @return the radio
+	 * Método de tipo get que nos devuelve el radio de la circunferencia
+	 * @return radio, el radio de la circunferencia
 	 */
 	public double getRadio() {
 		return radio;
 	}
 
 	/**
-	 * @param radio the radio to set
+	 * Método de tipo set que establece el radio de la circunferencia
+	 * @param rad establece en el objeto circunferencia correspondiente
 	 */
 	public void setRadio(double rad) {
 		this.radio = rad;
 	}
-	
+
 }
--- refactor/src/figuras/Test.java
+++ refactor/src/figuras/Test.java
@@ -1,6 +1,18 @@
 package figuras;
 
+/**
+ * Esta clase deonominada Test, es la clase principal del proyecto que básicamente compara el radio de 2 circunferencias
+ * 
+ * @author Oscar Domínguez, <a href="mailto:oscarluisdominguezrivero@alumno.ieselrincon.es">enviar correo electrónico</a>
+ */
 public class Test {
+	
+	/**
+	 * Es el método principal, main, del proyecto, y en él se crean tres objetos Circunferencia, aunque sólo
+	 * se compara la circunferencia 2 con la 3, mediante el método "esIgual" de la clase Circunferencia. 
+	 * la circunferecia 1 no se usa en el resto del código, por lo que podría ser borrada.
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		Circunferencia c1 = new Circunferencia(5.5);
 		Circunferencia c2 = new Circunferencia(10.1);
