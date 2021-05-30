e365cd2d36e6ee1b6ebec0be56a9e3dff274ebeb
oscar <oscarluisdominguezrivero@alumno.ieselrincon.es>
2021-05-30 10:11:53 +0100

    Cambiar la firma del método esIgual, apartado k.

--- refactor/src/figuras/Circunferencia.java
+++ refactor/src/figuras/Circunferencia.java
@@ -23,10 +23,10 @@ public class Circunferencia {
 		return area;
 	}
 
-	public boolean esIgual(Circunferencia otro, boolean conDecimales) {
+	public boolean esIgual(boolean considerarDecimales, Circunferencia otro) {
 		double radio1 = this.radio;
 		double radio2 = otro.getRadio();
-		if (conDecimales) {
+		if (considerarDecimales) {
 			if (radio1 == radio2)
 				return true;
 			else
